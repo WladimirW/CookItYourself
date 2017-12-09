d728cce219ac000d85e7fb32477984ddd856716e
Patrick Petrovic <ppati000@me.com>
2017-12-09 14:23:59 +0100

    Second fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -50,7 +50,7 @@ router.post("/recipeNames2", rawParser, (req, res, next) => {
 router.post("/recipeNames", (req, res, next) => {
   let labelResults;
 
-  getLabels("../../client/" + req.body.imageUrl)
+  getLabels(_.startsWith(req.body.imageUrl, "http") ? req.body.imageUrl : "../../client/" + req.body.imageUrl)
     .then(results => {
       labelResults = results || [];
 
